70a2db7a70b7b67b1aad12017cf9c63c6f935337
lg <lg@pyou.com>
2022-11-25 22:49:40 +0800

    test

--- LGH265/ViewController.mm
+++ LGH265/ViewController.mm
@@ -35,6 +35,8 @@ extern "C" {
 
 @implementation ViewController
 
+FILE *fp_open;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupUI];
@@ -47,28 +49,114 @@ extern "C" {
 - (void)startDecodeByFFmpegWithIsH265Data:(BOOL)isH265 {
     NSString *path = [[NSBundle mainBundle] pathForResource:isH265 ? @"testh265" : @"testh264" ofType:@"MOV"];
     path = [[NSBundle mainBundle] pathForResource:@"test"  ofType:@"h265"];
+    //    XDXAVParseHandler *parseHandler = [[XDXAVParseHandler alloc] initWithPath:path];
+    //    XDXFFmpegVideoDecoder *decoder = [[XDXFFmpegVideoDecoder alloc] initWithFormatContext:[parseHandler getFormatContext] videoStreamIndex:[parseHandler getVideoStreamIndex]];
+    //    decoder.delegate = self;
+    //    [parseHandler startParseGetAVPackeWithCompletionHandler:^(BOOL isVideoFrame, BOOL isFinish, AVPacket packet) {
+    //
+    ////        AVProbeData probe_data;
+    ////        probe_data.buf_size = packet.size;
+    ////        probe_data.filename = "";
+    ////        probe_data.buf = packet.data;
+    ////        AVInputFormat *pAVInputFormat = av_probe_input_format(&probe_data, 1);
+    //
+    ////        open_input_buffer(packet.data, packet.size);
+    //
+    //        if (isFinish) {
+    //            [decoder stopDecoder];
+    //            return;
+    //        }
+    //
+    //        if (isVideoFrame) {
+    //            [decoder startDecodeVideoDataWithAVPacket:packet];
+    //        }
+    //    }];
+    
+    path = [[NSBundle mainBundle] pathForResource:@"unset_test"  ofType:@"h265"];
+    
+    NSData *data = [[NSData alloc] initWithContentsOfFile:path];
+    
+    //    fp_open = fopen("test1.h264","rb+");
+    //    AVFormatContext *ic = NULL;
+    //    ic = avformat_alloc_context();
+    //    int bufferSize = 32768;
+    //    unsigned char * iobuffer = (unsigned char *)av_malloc(bufferSize);
+    //    AVIOContext *avio = avio_alloc_context(iobuffer, bufferSize, 0, NULL, fill_iobuffer, NULL, NULL);
+    //    ic->pb = avio;
+    //    int ret = avformat_open_input(&ic, "nothing", NULL, NULL);
+    //    XDXFFmpegVideoDecoder *decoder = [[XDXFFmpegVideoDecoder alloc] initWithFormatContext:ic videoStreamIndex:0];
+    //    decoder.delegate = self;
+    //
+    //    AVPacket packet;
+    //    av_init_packet(&packet);
+    //    int size = av_read_frame(ic, &packet);
+    //    if (size < 0 || packet.size < 0) {
+    //
+    //        int a = 3;
+    //    }
+    
+    
+    
+    //    open_input_buffer((uint8_t *)[data bytes], data.length);
+    
+    //    unsigned char *avio_ctx_buffer = NULL;
+    //    size_t avio_ctx_buffer_size = len;
+    //
+    //    AVInputFormat* in_fmt = av_find_input_format("h265");
+    //
+    //    bd.ptr = buf;  /* will be grown as needed by the realloc above */
+    //    bd.size = len; /* no data at this point */
+    //
+    //    AVFormatContext *fmt_ctx = avformat_alloc_context();
+    //
+    //    avio_ctx_buffer = (unsigned char *)av_malloc(avio_ctx_buffer_size);
+    //
+    //    /* 读内存数据 */
+    //    AVIOContext *avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size, 0, NULL, read_packet, NULL, NULL);
+    //
+    //    fmt_ctx->pb = avio_ctx;
+    //    fmt_ctx->flags = AVFMT_FLAG_CUSTOM_IO;
+    //
+    //    /* 打开内存缓存文件, and allocate format context */
+    //    if (avformat_open_input(&fmt_ctx, "", in_fmt, NULL) < 0) {
+    //        fprintf(stderr, "Could not open input\n");
+    //        return ;
+    //    }
+    
+    
     XDXAVParseHandler *parseHandler = [[XDXAVParseHandler alloc] initWithPath:path];
-    XDXFFmpegVideoDecoder *decoder = [[XDXFFmpegVideoDecoder alloc] initWithFormatContext:[parseHandler getFormatContext] videoStreamIndex:[parseHandler getVideoStreamIndex]];
-    decoder.delegate = self;
-    [parseHandler startParseGetAVPackeWithCompletionHandler:^(BOOL isVideoFrame, BOOL isFinish, AVPacket packet) {
+    [parseHandler startParseGetAVPackeWithCompletionHandler:^(BOOL isVideoFrame, BOOL isFinish, AVPacket packet1) {
         
-//        AVProbeData probe_data;
-//        probe_data.buf_size = packet.size;
-//        probe_data.filename = "";
-//        probe_data.buf = packet.data;
-//        AVInputFormat *pAVInputFormat = av_probe_input_format(&probe_data, 1);
-
-//        open_input_buffer(packet.data, packet.size);
+        uint8_t *buf = packet1.data;
+        int len = packet1.size;
         
-        if (isFinish) {
-            [decoder stopDecoder];
+        AVFormatContext *fmt_ctx = avformat_alloc_context();
+        
+        open_input_buffer(&fmt_ctx, buf, len);
+        
+        AVPacket packet;
+        av_init_packet(&packet);
+        int size = av_read_frame(fmt_ctx, &packet);
+        if (size < 0 || packet.size < 0) {
+            int a = 3;
             return;
         }
-        
-        if (isVideoFrame) {
-            [decoder startDecodeVideoDataWithAVPacket:packet];
-        }
+        XDXFFmpegVideoDecoder *decoder = [[XDXFFmpegVideoDecoder alloc] initWithFormatContext:fmt_ctx videoStreamIndex:0];
+        decoder.delegate = self;
+        [decoder startDecodeVideoDataWithAVPacket:packet];
+        av_packet_unref(&packet);
     }];
+    
+    
+}
+
+int fill_iobuffer(void * opaque,uint8_t *buf, int bufsize) {
+    if(!feof(fp_open)){
+        int true_size=fread(buf,1,bufsize,fp_open);
+        return true_size;
+    }else{
+        return -1;
+    }
 }
 
 /*正确方式*/
@@ -100,7 +188,7 @@ int read_packet(void *opaque, uint8_t *buf, int buf_size)
 }
 
 /* 打开前端传来的视频buffer */
-int open_input_buffer(uint8_t *buf, int len)
+int open_input_buffer(AVFormatContext **fmt_ctx, uint8_t *buf, int len)
 {
     unsigned char *avio_ctx_buffer = NULL;
     size_t avio_ctx_buffer_size = len;
@@ -110,18 +198,18 @@ int open_input_buffer(uint8_t *buf, int len)
     bd.ptr = buf;  /* will be grown as needed by the realloc above */
     bd.size = len; /* no data at this point */
 
-    AVFormatContext *fmt_ctx = avformat_alloc_context();
+    AVFormatContext *fmt_ctx1 = *fmt_ctx;
 
     avio_ctx_buffer = (unsigned char *)av_malloc(avio_ctx_buffer_size);
 
     /* 读内存数据 */
     AVIOContext *avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size, 0, NULL, read_packet, NULL, NULL);
 
-    fmt_ctx->pb = avio_ctx;
-    fmt_ctx->flags = AVFMT_FLAG_CUSTOM_IO;
+    fmt_ctx1->pb = avio_ctx;
+    fmt_ctx1->flags = AVFMT_FLAG_CUSTOM_IO;
 
     /* 打开内存缓存文件, and allocate format context */
-    if (avformat_open_input(&fmt_ctx, "", in_fmt, NULL) < 0)
+    if (avformat_open_input(&fmt_ctx1, "", in_fmt, NULL) < 0)
     {
         fprintf(stderr, "Could not open input\n");
         return -1;
